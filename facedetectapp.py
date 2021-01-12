720b0887f9530f0eb71bf4c4bea7f8212973b4f7
Blair Jones <bjonesneu@gmail.com>
2021-01-12 17:46:32 -0600

    Working detection.  Before adding MQTT publish.

--- facedetectapp.py
+++ facedetectapp.py
@@ -9,27 +9,32 @@ while(True):
     ret, frame = cap.read()
 
     # Remove color information to compact data
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     # Display the resulting frame
-    #cv.imshow('frame',gray)
+    #cv.imshow('frame',frame_gray)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # print('gray',gray)
+    # print('grayframe',frame_gray)
     if True:
         # face detection and other logic goes here
         face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
         for (x,y,w,h) in faces:
             print('face detected at', x, y, w, h)
-            # your logic goes here; for instance
-            # cut out face from the frame.. 
-            # rc,png = cv.imencode('.png', face)
+            cv.rectangle(frame_gray, (x, y), (x+w, y+h), (255, 255, 255), 2)
+            face = frame_gray[y:y+h,x:x+w]
+            #cv.imshow('frame',face)
+            # cut out face from the frame... 
+            rc,png = cv.imencode('.png', face)
             # msg = png.tobytes()
             # ...
-        
+
+    cv.imshow('frame',frame_gray)
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
+
